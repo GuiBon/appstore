e3fe79bd791d15c7bd8ba8d9a15244d32c1e649b
Guillaume Bonin <1guillaume.bonin@gmail.com>
2017-10-27 11:56:48 +0200

    Minimal style on search

--- client/app/components/Home/Search.jsx
+++ client/app/components/Home/Search.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Grid, Row, Col, ButtonToolbar, Button} from 'react-bootstrap';
 import {InstantSearch, Hits, SearchBox, Highlight, RefinementList, Pagination, CurrentRefinements, ClearAll} from 'react-instantsearch/dom'
+
+import style from './Search.scss'
 
 export default class Search extends React.Component {
 
@@ -18,30 +20,27 @@ export default class Search extends React.Component {
   appDisplay({hit}) {
     return (
       <Grid>
-        <Row className='hit'>
-          <Col xs={4} md={4} className='hit-image'>
+        <Row className={style.hit}>
+          <Col xs={2} md={2} className='hit-image'>
             <img src={hit.image} />
           </Col>
-          <Col xs={4} md={4} className='hit-content'>
+          <Col xs={8} md={8} className='hit-content'>
             <Row className='hit-name'>
               <Highlight attributeName='name' hit={hit} />
             </Row>
             <Row className='hit-info'>
-              {hit.price} € - {hit.rating} stars
+              {hit.price} € - {hit.rating} ☆
             </Row>
           </Col>
-          <Col xs={2} md={2} className='icon-edit'>
-            <button name='editButton' >
-              Edit
-            </button>
-          </Col>
           <Col xs={2} md={2} className='icon-delete'>
-            <button name='deleteButton' onClick={() => { 
-              if (confirm('Delete this app ?')) {
-                this.deleteApp(hit.objectID);
-              }}}>
-              Delete
-            </button>
+            <ButtonToolbar>
+              <Button bsStyle='danger' name='deleteButton' onClick={() => { 
+                if (confirm('Delete this app ?')) {
+                  this.deleteApp(hit.objectID);
+                }}}>
+                Delete
+              </Button>
+            </ButtonToolbar>
           </Col>
         </Row>
       </Grid>
@@ -69,20 +68,24 @@ export default class Search extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <button name='backButton' onClick={this.handleBack}>
+        <ButtonToolbar className={style.backButton}>
+          <Button name='backButton' onClick={this.handleBack} bsStyle='primary'>
             Back
-          </button>
-        </div>
+          </Button>
+        </ButtonToolbar>
         <InstantSearch
           appId='LZ6T2ZYQUS'
           apiKey='46edf7593a0474c6207ba8d01c5f7ddd'
           indexName='App'
         >
-        <CurrentRefinements/>
-        <ClearAll/>
         <SearchBox />
-        <RefinementList attributeName='genres' />
+        <p>{' '}</p>
+        <CurrentRefinements />
+        <p>{' '}</p>
+        <ClearAll/>
+        <p>{' '}</p>
+        <RefinementList showMore={true} className={style.refinement} attributeName='genres' />
+        <p>{' '}</p>
         <Hits hitComponent={this.appDisplay}/>
         <Pagination />
         </InstantSearch>
